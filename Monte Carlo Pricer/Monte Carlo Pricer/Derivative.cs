de8d464929d86504d4cdc9cf4427b6eef1afd5b5
WhitneyRueckl <32363818+WhitneyRueckl@users.noreply.github.com>
2020-02-27 19:06:33 -0600

    5092 Project 1 Submission

---
+++ Monte Carlo Pricer/Monte Carlo Pricer/Derivative.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monte_Carlo_Pricer
+{
+    class Derivative: FinancialInstrument
+    {
+
+
+
+    }
+}
